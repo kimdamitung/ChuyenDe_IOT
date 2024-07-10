1174c678ef3f21afafd77b63f7c8d905b8b7a517
kimdamitung <150353472+kimdamitung@users.noreply.github.com>
2024-07-10 23:04:58 +0700

    Update code nhóm 1 và 7 mức độ 3 10 điểm

--- Chuong_01/ThiNghiem/MucDo3/src/main.py
+++ Chuong_01/ThiNghiem/MucDo3/src/main.py
@@ -5,35 +5,38 @@ from time import sleep, sleep_us, sleep_ms
 from sys import exit
 
 def set_level_gpio_output(gpio, state):
+	# state with 0: ON, 1: OFF
 	if not ((1 <= gpio <= 21) or (35 <= gpio <= 45) or gpio in [47, 48]):
 		print("Hãy nhập GPIO NUMBER từ 1 - 21, 35 - 45, 47, 48")
 		exit()
-	if state not in (0, 1, False, True):
+	if state not in (0, 1):
 		print("Hãy nhập GPIO LEVEL là 0 (False) và 1 (True)")
 		exit()
 	gpio_t = Pin(gpio, Pin.OUT)
-	if state in (0, False):
-		gpio_t.value(1)
-	elif state in (1, True):
-		gpio_t.value(0)
+	if state in (0, 1):
+		gpio_t.value(state) 
 
 def check_state(dec, hex):
-	if(dec & hex) != 0:
-		return True
-	return False
+	if(dec & hex):
+		return 0
+	return 1
 
-count = 0
+count = 255
+
+print("Nhóm 1, 7")
 
 while True:
-	set_level_gpio_output(4, check_state(count, 0x80))
-	set_level_gpio_output(5, check_state(count, 0x40))
-	set_level_gpio_output(6, check_state(count, 0x20))
-	set_level_gpio_output(7, check_state(count, 0x10))
-	set_level_gpio_output(15, check_state(count, 0x08))
-	set_level_gpio_output(16, check_state(count, 0x04))
-	set_level_gpio_output(17, check_state(count, 0x02))
-	set_level_gpio_output(18, check_state(count, 0x01))
+	set_level_gpio_output(4, check_state(count, 128))
+	set_level_gpio_output(5, check_state(count, 64))
+	set_level_gpio_output(6, check_state(count, 32))
+	set_level_gpio_output(7, check_state(count, 16))
+	set_level_gpio_output(15, check_state(count, 8))
+	set_level_gpio_output(16, check_state(count, 4))
+	set_level_gpio_output(17, check_state(count, 2))
+	set_level_gpio_output(18, check_state(count, 1))
+	print(f"{check_state(count, 1)} {check_state(count, 2)} {check_state(count, 4)} {check_state(count, 8)} {check_state(count, 16)} {check_state(count, 32)} {check_state(count, 64)} {check_state(count, 128)}")
 	sleep(1)
-	count += 1
-	if count == 256:
-		count = 0
+	count -= 1
+	if count < 0:
+		print("END")
+		count = 255
